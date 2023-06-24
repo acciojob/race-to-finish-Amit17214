be8568fd9bc2f0c8c3dbf0abeca0e6d74f5ebd48
Amit17214 <120364136+Amit17214@users.noreply.github.com>
2023-06-24 18:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 window.promises = [];
 
 let a = new Promise(resolve => {
-	setTimeOut(() => {
+	setTimeout(() => {
 		resolve("a")
 	}, 3000)
 })
 let b = new Promise(resolve => {
-	setTimeOut(() => {
+	setTimeout(() => {
 		resolve("b")
 	}, 2000)
 })
 let c = new Promise(resolve => {
-	setTimeOut(() => {
+	setTimeout(() => {
 		resolve("c")
 	}, 1000)
 })
 let d = new Promise(resolve => {
-	setTimeOut(() => {
+	setTimeout(() => {
 		resolve("d")
 	}, 4000)
 })
 let e = new Promise(resolve => {
-	setTimeOut(() => {
+	setTimeout(() => {
 		resolve("e")
 	}, 5000)
 })
 let x = Promise.any([a, b, c, d, e])
-windows.promises = [a, b, c, d, e];
+window.promises = [a, b, c, d, e];
 x.then((data) => {
 	const element = document.getElementById("output");
 	element.innerText = data ;
